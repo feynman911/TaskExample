35ac11158c8b0b1233097d14eaf3d860a99cf7ce
feynman911 <feynman911@gmail.com>
2019-03-27 21:45:51 +0900

    プロジェクト ファイルを追加します。

---
+++ TaskExample/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,177 @@
+﻿using Prism.Mvvm;
+using Prism.Commands;
+using System;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TaskExample.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Task & Lock";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+
+        }
+
+        Object lockObj = new Object();
+
+        private bool cancel1 = false;
+        public bool Cancel1
+        {
+            get { return cancel1; }
+            set { SetProperty(ref cancel1, value); }
+        }
+
+        private bool cancel2 = false;
+        public bool Cancel2
+        {
+            get { return cancel2; }
+            set { SetProperty(ref cancel2, value); }
+        }
+
+        private bool task1run = false;
+        public bool Task1run
+        {
+            get { return task1run; }
+            set { SetProperty(ref task1run, value);
+                CommandTask1.RaiseCanExecuteChanged();
+                CommandTask1Cancel.RaiseCanExecuteChanged();
+            }
+        }
+
+        private bool task2run = false;
+        public bool Task2run
+        {
+            get { return task2run; }
+            set { SetProperty(ref task2run, value);
+                CommandTask2.RaiseCanExecuteChanged();
+                CommandTask2Cancel.RaiseCanExecuteChanged();
+            }
+        }
+
+        private int tValue = 30;
+        /// <summary>
+        /// プログレスバー表示用
+        /// </summary>
+        public int TValue
+        {
+            get { return tValue; }
+            set { SetProperty(ref tValue, value); }
+        }
+
+        public void Task1()
+        {
+            lock (lockObj)
+            {
+                while (TValue < 100)
+                {
+                    //ダミー負荷用ウエイトms スレッドを止める
+                    Thread.Sleep(30);
+                    //状況の報告
+                    TValue++;
+                    //キャンセルリクエストの確認
+                    if (Cancel1)
+                    {
+                        break;
+                    }
+                }
+            }
+            Cancel1 = false;
+        }
+
+        public void Task2()
+        {
+            lock (lockObj)
+            {
+                while (TValue > 0)
+                {
+                    //ダミー負荷用ウエイトms スレッドを止める
+                    Thread.Sleep(30);
+                    //状況の報告
+                    TValue--;
+                    //キャンセルリクエストの確認
+                    if (Cancel2)
+                    {
+                        break;
+                    }
+                }
+            }
+            Cancel2 = false;
+        }
+
+        #region CommandTask1
+        private DelegateCommand commandTask1;
+        public DelegateCommand CommandTask1 =>
+            commandTask1 ?? (commandTask1 = new DelegateCommand(ExecuteCommandTask1, CanExecuteCommandTask1));
+
+        async void ExecuteCommandTask1()
+        {
+            Task1run = true;
+            await Task.Run(() => Task1());
+            Task1run = false;
+        }
+
+        bool CanExecuteCommandTask1()
+        {
+            return !Task1run;
+        }
+        #endregion
+
+        #region CommandTask2
+        private DelegateCommand commandTask2;
+        public DelegateCommand CommandTask2 =>
+            commandTask2 ?? (commandTask2 = new DelegateCommand(ExecuteCommandTask2, CanExecuteCommandTask2));
+
+        async void ExecuteCommandTask2()
+        {
+            Task2run = true;
+            await Task.Run(() => Task2());
+            Task2run = false;
+        }
+
+        bool CanExecuteCommandTask2()
+        {
+            return !Task2run;
+        }
+        #endregion
+
+        #region CommandTask1Cancel
+        private DelegateCommand commandTask1Cancel;
+        public DelegateCommand CommandTask1Cancel =>
+            commandTask1Cancel ?? (commandTask1Cancel = new DelegateCommand(ExecuteCommandTask1Cancel, CanExecuteCommandTask1Cancel));
+
+        void ExecuteCommandTask1Cancel()
+        {
+            Cancel1 = true;
+        }
+
+        bool CanExecuteCommandTask1Cancel()
+        {
+            return Task1run;
+        }
+        #endregion
+
+        #region CommandTask2Cancel
+        private DelegateCommand commandTask2Cancel;
+        public DelegateCommand CommandTask2Cancel =>
+            commandTask2Cancel ?? (commandTask2Cancel = new DelegateCommand(ExecuteCommandTask2Cancel, CanExecuteCommandTask2Cancel));
+
+        void ExecuteCommandTask2Cancel()
+        {
+            Cancel2 = true;
+        }
+
+        bool CanExecuteCommandTask2Cancel()
+        {
+            return Task2run;
+        }
+        #endregion
+    }
+}
---
+++ TaskExample/Views/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Threading;
+
+namespace TaskExample.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            task1_cancel_button.IsEnabled = false;
+            task2_cancel_button.IsEnabled = false;
+        }
+
+        private CancellationTokenSource cancelTokensource1;//キャンセル判定用
+        private CancellationTokenSource cancelTokensource2;//キャンセル判定用
+        Object lockObj = new Object();
+        int percent = 0;
+
+        //Task実行用にIProgressとCancellationTokent追加
+        //複数変数戻したい時には独自クラスを定義
+        public bool Task1(IProgress<int> p, CancellationToken cancelToken)
+        {
+            bool ret = true;
+            lock(lockObj)
+            {
+                while (percent < 100)
+                {
+                    //ダミー負荷用ウエイトms スレッドを止める
+                    Thread.Sleep(30);
+                    //状況の報告
+                    percent++;
+                    p.Report(percent);
+                    //キャンセルリクエストの確認
+                    if (cancelToken.IsCancellationRequested)
+                    {
+                        ret = false;
+                        break;
+                    }
+                }
+            }
+            return ret;
+        }
+
+        public bool Task2(IProgress<int> p, CancellationToken cancelToken)
+        {
+            bool ret = true;
+            lock(lockObj)
+            {
+                while (percent > 0)
+                {
+                    //ダミー負荷用ウエイトms スレッドを止める
+                    Thread.Sleep(30);
+                    //状況の報告
+                    percent--;
+                    p.Report(percent);
+                    //キャンセルリクエストの確認
+                    if (cancelToken.IsCancellationRequested)
+                    {
+                        ret = false;
+                        break;
+                    }
+                }
+            }
+            return ret;
+        }
+
+        //textbox1 & ProgressBar1 の更新
+        public void SetText(int percent)
+        {
+            textBox1.Text = percent.ToString();
+            progressBar1.Value = percent;
+        }
+
+        private void Task1_cancel_button_Click(object sender, RoutedEventArgs e)
+        {
+            if (cancelTokensource1 != null) cancelTokensource1.Cancel();
+        }
+
+        private void Task2_cancel_button_Click(object sender, RoutedEventArgs e)
+        {
+            if (cancelTokensource2 != null) cancelTokensource2.Cancel();
+        }
+
+        private async void Task1_button_Click(object sender, RoutedEventArgs e)
+        {
+            task1_button.IsEnabled = false;
+            task1_cancel_button.IsEnabled = true;
+            var p = new Progress<int>(SetText);
+            cancelTokensource1 = new CancellationTokenSource();
+            var cToken = cancelTokensource1.Token;
+            bool ret = await Task.Run(() => Task1(p, cToken));
+            task1_button.IsEnabled = true;
+            task1_cancel_button.IsEnabled = false;
+            cancelTokensource1.Dispose();
+            cancelTokensource1 = null;
+        }
+
+        private async void Task2_button_Click(object sender, RoutedEventArgs e)
+        {
+            task2_button.IsEnabled = false;
+            task2_cancel_button.IsEnabled = true;
+            var p = new Progress<int>(SetText);
+            cancelTokensource2 = new CancellationTokenSource();
+            var cToken = cancelTokensource2.Token;
+            bool ret = await Task.Run(() => Task2(p, cToken));
+            task2_button.IsEnabled = true;
+            task2_cancel_button.IsEnabled = false;
+            cancelTokensource2.Dispose();
+            cancelTokensource2 = null;
+        }
+
+    }
+}
